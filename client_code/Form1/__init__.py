ad01f950398c74d746830522e4b7eaecb6d58780
dbayless@humanscalebusiness.org <dbayless@humanscalebusiness.org>
2023-11-25 19:02:43 +0000

    Edited Form1.ItemTemplate1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,57 +1,51 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 
 class Form1(Form1Template):
-
     def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
+        # Initialize form properties and components
         self.init_components(**properties)
 
-        # Initialize a new OpenAI thread
-        anvil.server.call('create_new_thread')
-
-        # Reset the conversation when the app starts     
-        self.conversation = anvil.server.call('reset_conversation')
-
-        # Update the interface with the current state of the conversation
-        self.refresh_conversation()
-
-    def refresh_conversation(self):
-        # Construct a list of messages
-        messages = []
-        for message in self.conversation:
-            role = message["role"]
-            text = message["value"]
-            if role == "user":
-                messages.append({"from": "user", "text": text})
-            elif role == "assistant":
-                messages.append({"from": "assistant", "text": text})
-
-        # Update the interface
-        self.repeating_panel_1.items = messages
-
-    def send_btn_click(self, **event_args):
-        """This method is called when the button is clicked"""
-        # Send the contents of the new message box to the server
-        self.conversation = anvil.server.call(
-            'send_message',
-            self.new_message_box.text,
-        )
-        
-        # Clear the contents of the new message box
-        self.new_message_box.text = ""
+        # Set up conversation
+        self.initialize_conversation()
 
         # Refresh the conversation display
         self.refresh_conversation()
 
-        # Scroll down to ensure the send message button is in view
+    def initialize_conversation(self):
+        """Initialize a new OpenAI thread and resets the conversation."""
+        anvil.server.call('create_new_thread')
+        self.conversation = anvil.server.call('reset_conversation')
+
+    def refresh_conversation(self):
+        """Updates the interface with the current state of the conversation."""
+        messages = self.format_conversation((self.conversation))
+        self.repeating_panel_1.items = messages
+
+    def format_conversation(self, conversation):
+        """Formats the conversation messages for display."""
+        return [{"from": message["role"], "text": message["value"]} for message in conversation]
+       
+    def send_btn_click(self, **event_args):
+        """Handles the event when the send button is clicked"""
+        self.send_message(self.new_message_box.text)
+        self.clear_message_box()
+        self.refresh_conversation()
+        self.scroll_to_bottom()
+
+    def send_message(self, message):
+        """Sends a message to the server."""
+        self.conversation = anvil.server.call(send_message', message))
+
+    def clear_message_box(self):
+        """Clears the new message input box."""
+        self.new_message_box.text = ""
+
+    def scroll_to_bottom(self):
+        """Scrolls the view to the bottom."""
         self.send_btn.scroll_into_view()
-        
 
     def new_message_box_pressed_enter(self, **event_args):
-        """This method is called when the user presses Enter in this text box"""
+        """Triggered when Enter is pressed in the message box."""
         self.send_btn_click()
